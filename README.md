8744cded02008f6fe4d5fc2801c2f1b484cf778e
修伟 <xiuwei@sunshineftg.com>
2021-11-23 15:57:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# utools_media_control
+- 播放,暂停,停止,上一曲,下一曲,静音
---
+++ preload.js
@@ -0,0 +1,77 @@
+window.exports = {
+    "next": { // 注意：键对应的是 plugin.json 中的 features.code
+        mode: "none", // 用于无需 UI 显示，执行一些简单的代码
+        args: {
+            // 进入插件时调用
+            enter: (action) => {
+                // action = { code, type, payload }
+                window.utools.hideMainWindow();
+                // do some thing
+
+                // utools.simulateKeyboardTap("MediaPlayPause"); //暂停
+                // utools.simulateKeyboardTap("MediaPreviousTrack"); //上一曲
+                utools.simulateKeyboardTap("MediaNextTrack"); //下一曲
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "previous": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("MediaPreviousTrack"); //上一曲
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "pause": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("MediaPlayPause"); //暂停
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "volumeup": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("VolumeUp"); //音量加
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "volumedown": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("VolumeDown"); //音量减
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "mute": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("VolumeMute"); //静音
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "sound": {
+        mode: 'list',
+        args: {
+            placeholder: "输入数字,调整音量 0-100",
+            enter: (action) => {
+                alert(233);
+            }
+        }
+    }
+}

2411e433fe9ea7378367d4a84a0aa215eb6aa841
fortystory <forty40@live.cn>
2021-11-30 19:05:06 +0800

    modfiy:添加音量控制;添加.gitignore;添加package.json

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # utools_media_control
 - 播放,暂停,停止,上一曲,下一曲,静音
+
+
+安装`loudness`
+`npm install`
+
+将`plugin.json`utools开发者工具
--- preload.js
+++ preload.js
@@ -1,4 +1,7 @@
-// const cp = require('child_process') //调用系统命令 cp.exec('notepad');
+const cp = require('child_process') //调用系统命令 cp.exec('notepad');
+const loudness = require('loudness')
+
+
 window.exports = {
     "next": { // 注意：键对应的是 plugin.json 中的 features.code
         mode: "none", // 用于无需 UI 显示，执行一些简单的代码
@@ -55,27 +58,68 @@ window.exports = {
                 window.utools.outPlugin();
             }
         }
+    },
+    "mute": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow();
+                utools.simulateKeyboardTap("VolumeMute"); //静音
+                window.utools.outPlugin();
+            }
+        }
+    },
+    "sound": {
+        mode: 'list',
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                // 如果进入插件就要显示列表数据
+                callbackSetList([{
+                    title: '调整音量',
+                    description: '输入数字,调整音量 0-100',
+                    icon: 'icons/Soundshengyin4.png' // 图标(可选)
+                }])
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                // 获取一些数据
+                // 执行 callbackSetList 显示出来
+                callbackSetList([{
+                    title: '调整音量',
+                    description: '调整为:' + searchWord,
+                    sound_val: searchWord,
+                    icon: 'icons/Soundshengyin4.png', // 图标
+                    // url: 'https://yuanliao.info'
+                }])
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow();
+                // let vol = await loudness.getVolume();
+                let vol = loudness.getVolume();
+                console.log(vol);
+
+                utools.simulateKeyboardTap("VolumeUp");
+
+                let sound_val = itemData.sound_val
+                if (sound_val > 100) {
+                    sound_val = 100;
+                }
+                if (sound_val < 0) {
+                    sound_val = 0;
+                }
+
+                if (itemData.sound_val == 0) {
+                    // utools.simulateKeyboardTap("VolumeMute");
+                    cp.exec(loudness.setMuted(true));
+                } else {
+                    cp.exec(loudness.setVolume(sound_val));
+                }
+                window.utools.outPlugin();
+            },
+            // 子输入框为空时的占位符，默认为字符串"搜索"
+            placeholder: "输入音量数值 0-100"
+        }
     }
-    // ,
-    // "mute": {
-    //     mode: "none",
-    //     args: {
-    //         enter: (action) => {
-    //             window.utools.hideMainWindow();
-    //             // utools.simulateKeyboardTap("VolumeMute"); //静音
-    //             cp.exec('setvol 10');
-    //             alert(2333);
-    //             window.utools.outPlugin();
-    //         }
-    //     }
-    // }
-    // ,
-    // "sound": {
-    //     mode: 'list',
-    //     args: {
-    //         placeholder: "输入数字,调整音量 0-100",
-    //         enter: (action) => {
-    //         }
-    //     }
-    // }
 }
